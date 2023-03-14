c30971157c6f43e2e0885c66e41491d023fe2c32
xull <xull@hexmeet.com>
2023-03-14 18:10:35 +0800

    update doc

--- simple_buffer_cut.c
+++ simple_buffer_cut.c
@@ -7,6 +7,7 @@
 #define NEW_LINE 30
 #define SILENCE_STEP 50
 
+//帮助函数
 int help(char *argv[])
 {
     printf("please input a pcm file like:%s a.pcm samplerate framesize silencestep\n", argv[1]);
@@ -18,8 +19,12 @@ int main(int argc, char* argv[])
     if(argc < 2){
         return help(argv);
     }
+    
+    //每次塞入buffer大小
     int frameSize = MAX_FRAME_SIZE;
+    //静音检测，间隔多少个silence才舍弃当前音频段
     int silenceStep = SILENCE_STEP;
+    //采样率
     short sampleRate = 16000;
 
     if(argc == 3) sampleRate = atoi(argv[2]);
@@ -31,18 +36,21 @@ int main(int argc, char* argv[])
     short *frame = (short*)malloc(frameSize*sizeof(short));
     int status = 0;
 
+    // 创建句柄
     handle = WebRtcVad_Create();
     if(handle == NULL){
         printf("webrtc vad create error%d\n", status);
         return status;
     }
 
+    // 初始化vad
     status = WebRtcVad_Init(handle);
     if(status){
         printf("webrtc vad init error:%d\n", status);
         return status;
     }
 
+    // 设置mode
     status = WebRtcVad_set_mode(handle, 3);
     if(status){
         printf("webrtc vad mode set error:%d\n", status);
@@ -64,6 +72,7 @@ int main(int argc, char* argv[])
             if(len > 0){
                 inx++;
                 printf("index:%d len:%d sizeof(short):%ld\n", inx, len, sizeof(short));
+                // 处理pcm数据
                 status = WebRtcVad_Process(handle, sampleRate, frame, len);
                 if(status == -1){
                     printf("webrtc vad process is error:%d\n", status);
@@ -77,11 +86,14 @@ int main(int argc, char* argv[])
                         }
                         char sentense[24];
                         sprintf(sentense, "sentence_%d.pcm", cursen);
+                        //  打开新的sentence 句子 
                         fpelber = fopen(sentense, "wb");
                         printf("new sentence:%d\n", cursen++);
                     }
 
                     step = 0;
+
+                    //  写入处理过的数据
                     fwrite(frame, sizeof(short), len, fpbf);
                     fwrite(frame, sizeof(short), len, fpelber);
                 }else{
@@ -99,6 +111,8 @@ int main(int argc, char* argv[])
     if(fpin != NULL)fclose(fpin);
     fclose(fpout);
     fclose(fpbf);
+
+    //  释放句柄
     WebRtcVad_Free(handle);
     return 0;
 }
